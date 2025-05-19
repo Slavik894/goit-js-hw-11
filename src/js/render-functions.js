d325786fe2610aedebd1fc0db96b1a4c4c8f4104
Slavik <slavik1058@gmail.com>
2025-05-20 00:19:27 +0200

    gallery markup and basic styles

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,9 +1,37 @@
-function createGallery(images){
+const gallery = document.querySelector(".gallery");
+
+export function createGallery(images){
+   
+    const galleryElement = images.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads})=>
+    `<li class="gallery-element">
+    <img class="gallery-element-img" src=${webformatURL} alt=${tags}>
+    <div class="element-description">
+    <ul class="description-list">
+        <li>
+        <p>Likes</p>${likes}
+        </li>
+        <li>
+        <p>Views</p>${views}
+        </li>
+        <li>
+        <p>Comments</p>${comments}
+        </li>
+        <li>
+        <p>Downloads</p>${downloads}
+        </li>
+    </ul>
+    
+    </div>
+    
+    </li>`
+    
+    );
+    gallery.insertAdjacentHTML("beforeend", galleryElement.join(''));
 
 };
 
-function clearGallery(){
-
+export function clearGallery(){
+    gallery.innerHTML = '';
 };
 
 function showLoader(){
@@ -11,5 +39,5 @@ function showLoader(){
 };
 
 function hideLoader(){
-    
+
 }
---
+++ src/main.js
@@ -0,0 +1,29 @@
+import { getImagesByQuery } from './js/pixabay-api.js';
+import {clearGallery, createGallery} from './js/render-functions.js';
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const query = form.elements['search-text'].value.trim();
+  if (!query) return;
+
+  clearGallery();
+  getImagesByQuery(query)
+    .then(response => {
+      const images = response.data.hits;
+      if (images.length > 0) {
+        createGallery(images);
+      }
+      iziToast.show({
+        message: "Sorry, there are no images matching your search query. Please try again!"
+      })
+      
+    })
+    .catch(console.error());
+    
+    
+});
